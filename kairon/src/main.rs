34e7f4c2235a4e260c43b39271ff3f2bebac59a2
SylvaGX <tomascsilvapro@gmail.com>
2023-12-20 08:52:24 +0000

    Changes on main.rs

--- kairon/src/main.rs
+++ kairon/src/main.rs
@@ -6,10 +6,10 @@ use winit::{
 
 fn main() {
     env_logger::init(); // Necessary for logging within WGPU
-    let event_loop = EventLoop::new(); // Loop provided by winit for handling window events
+    let event_loop = EventLoop::new().unwrap(); // Loop provided by winit for handling window events
+
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
 
     // Opens the window and starts processing events (although no events are handled yet)
-    event_loop.run(move |event, window_target, control_flow| {
-        let window = WindowBuilder::new().build(window_target).unwrap();
-    });
+    event_loop.run(move |event, _, control_flow| {});
 }
